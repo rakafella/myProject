f8085b85a31ca92502fa7e6bd34df6ca597dade5
Aleksey <rakafella25@gmail.com>
2023-06-19 21:58:45 +0300

    create_API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require("method-override");
 const postRoutes = require('./routes/post-routes');
+const postApiRoutes = require('./routes/api-post-routes');
 const contactRoutes = require('./routes/contact-routes');
 const createPath = require('./helpers/create-path');
 
@@ -15,33 +16,34 @@ const PORT = 3000;
 const db = 'mongodb+srv://rakafella25:Drota987456321@cluster0.qn7nvw9.mongodb.net/myproject'
 
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((res) => console.log('Connect to DB'))
-    .catch((error) => console.log(error));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
 
 app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.use(morgan(':method :url :status: res[content-length] - :response-time ms'));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use(express.static('styles'));
 
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-    const title = 'Home'
-    res.render(createPath('index'), { title });
+  const title = 'Home';
+  res.render(createPath('index'), { title });
 });
 
 app.use(postRoutes);
 app.use(contactRoutes);
+app.use(postApiRoutes);
 
 app.use((req, res) => {
-    const title = 'Error Page'
-    res
+  const title = 'Error Page';
+  res
     .status(404)
     .render(createPath('error'), { title });
 });
